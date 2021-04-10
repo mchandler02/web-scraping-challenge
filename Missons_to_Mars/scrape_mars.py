f9edaba0f8b1a88a36db295de5e46d076f3c8c6f
Molly Sullivan <mchandler02@gmail.com>
2021-04-09 20:21:42 -0500

    root route created

--- Missons_to_Mars/scrape_mars.py
+++ Missons_to_Mars/scrape_mars.py
@@ -7,11 +7,29 @@ import time
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 
+app = Flask(__name__)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-def scrape():
-    scrape_dict = {}
+@app.route("/")
+def index():
+    listings = mongo.db.listings.find()
+    return render_template("index.html", listings=listings)
+
+@app.route("/scrape")
+def scraper():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless = False)
+    listings = mongo.db.listings
+    listings_data = scrape(browser)
+    listings.update({}, listings_data, upsert=True)
+    browser.quit()
+
+    return redirect("/", code=302)
+
+
+def scrape(browser):
+    scrape_dict = {}
+
 
     url = "https://redplanetscience.com/"
     browser.visit(url)
@@ -80,10 +98,10 @@ def scrape():
         hemisphere_dict["img_url"] = image_url
         hemisphere_image_urls.append(hemisphere_dict)
 
-    browser.quit()
+    
     # print(hemisphere_image_urls)
     scrape_dict["hemisphere_image_urls"]=hemisphere_image_urls
     return scrape_dict
 
 if __name__ == "__main__":
-    scrape()
+    app.run(debug=True)
